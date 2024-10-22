f5cff79cda774e2abce5d315b4c5812385d84ac0
Trustacean <pizzacrustacean15@gmail.com>
2024-10-22 19:34:27 +0700

    new rgb to hex

--- 5-kyu/rgb-to-hex-conversion/src/main/java/trustacean/RgbToHex.java
+++ 5-kyu/rgb-to-hex-conversion/src/main/java/trustacean/RgbToHex.java
@@ -8,9 +8,25 @@ package trustacean;
  *
  * @author Trustacean
  */
-public class RgbToHex {
 
+public class RgbToHex {
     public static String rgb(int r, int g, int b) {
-        return null;
+        String hex = "";
+        hex += toHex(r);
+        hex += toHex(g);
+        hex += toHex(b);
+        return hex;
+    }
+
+    private static String toHex(int r) {
+        String hex = "";
+        if (r <= 0) {
+            hex += "00";
+        } else if (r >= 255) {
+            hex += "FF";
+        } else {
+            hex += Integer.toHexString(r).toUpperCase();
+        }
+        return hex.length() > 1 ? hex : "0" + hex;
     }
 }
